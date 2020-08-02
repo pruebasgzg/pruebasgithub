390cb17e2597c07b30bdb6512ba1ee210cece6b2
pruebasgzg <gustavozunigagoni@yahoo.com>
2020-08-02 00:08:34 -0600

    dato de nuevo usuario

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ var resultado = 2 + 2
 console.log(resultado)
 console.log("esta es una entrada nueva")
 console.log("este es un cambio para ver si funciona la coneccion con ssh a github")
+console.lof("dado de nuevo usuario")
